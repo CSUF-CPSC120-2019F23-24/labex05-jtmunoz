60169c4ac568b2435c71231a970a3e3b15bf4cc9
jtmunoz <jordantmunoz@gmail.com>
2019-09-21 14:33:42 -0700

    completes assignment

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,1 +1,29 @@
 // Random Rectangle
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+
+int main()
+{
+    // Declare variables & constants
+    std::string rect, row;
+    const int MAX_HEIGHT = 3;
+    const int MAX_LENGTH = 40;
+
+    // Set seed time
+    unsigned seed = time(0);
+    srand(seed);
+
+    // Random Length based off Seed time
+    int length = rand() % MAX_LENGTH + 1 ;
+
+    //Loop to create rectangle
+    for (int i = 0; i<MAX_HEIGHT; ++i) {
+      rect += row.assign(length, '#') + "\n";
+    }
+
+    // Print out rectangle
+    std::cout << std::endl << rect << std::endl;
+    return 0;
+}
